9cec8949cb0e17b65753b1457afb9f20a9a94a98
kyosuke36 <futianjingyou575@gmail.com>
2024-10-24 12:20:07 +0900

    調整くん作成 #1

--- event/views.py
+++ event/views.py
@@ -3,3 +3,8 @@ from django.views.generic import TemplateView
 
 class IndexView(TemplateView):
     template_name = "event/index.html"
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["title"] = "３０秒でつくれる！カンタン出欠表"
+        return context
